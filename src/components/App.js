eddaaaac7372ca33797659fc03df011460217da8
ashish <ashishmasih92g@gmail.com>
2025-06-10 11:01:49 +0530

    initial a submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,68 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [input, setInput] = useState({
+    username: "",
+    password: ""
+  })
+  let [error, setError] = useState("")
+  let { username, password } = input
+  
+
+  function implementChange(e) {
+    let { name, value } = e.target;
+    let updated ={ ...input, [name]: value } 
+    setInput(updated)
+    if(updated.username && updated.password){
+      setError("")
+    }
+
+  }
+
+
+
+  function validation() {
+
+    let errorMessage = ""
+   if(!username&& !password){
+    setError("both field are required")
+    return false
+   }
+   else if(!username){
+setError("username must not be empty")
+    return false 
+  }
+   else if(!password){
+  setError("password must not be empty")
+    return false
+   }
+   setError("")
+  return true
+
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (validation()) {
+      alert("form submitted successfully")
+    }
+
+
+  }
   return (
+
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={handleSubmit} >
+        <input type="text" name="username" id="username" onChange={implementChange} value={username} />
+        <input type="password" name="password" id="password" onChange={implementChange} value={password} />
+        <button>Submit</button>
+      </form>
+  {error&& <p id="errorMessage" style={{color:"red"}}>{error}</p>}
     </div>
+
   )
 }
 
